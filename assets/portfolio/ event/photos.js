0cdef0c834e590ab9500daf9b3e277b9036711a7
Mattis Roy <mattisroy@MacBook-Air-de-Mattis.local>
2026-04-17 19:32:21 +0200

    Add masonry showroom layout with dense gap filling

---
+++ assets/portfolio/ event/photos.js
@@ -0,0 +1,5 @@
+window.SHOWROOM_PHOTOS = [
+  "IMG_0001.webp",
+  "IMG_0002.webp",
+  "IMG_0003.webp"
+];
---
+++ assets/portfolio/dj/photos.js
@@ -0,0 +1,33 @@
+window.SHOWROOM_PHOTOS = [
+  "5.webp",
+  "8.webp",
+  "23.webp",
+  "18.webp",
+
+  "19.webp",
+  "21.webp",
+  "22.webp",
+  "25.webp",
+  "26.webp",
+  "0.5.webp",
+  "0.webp",
+
+  "27.jpg",
+  "IMG_2466 copie.jpg",
+  "16.webp",
+  "24.webp",
+  "11.webp",
+
+  "2.jpg",
+  "1.jpg",
+  "IMG_2265 copie.webp",
+
+  "IMG_2557 copie.jpg",
+  "IMG_2662 copie.webp",
+  "IMG_2668 copie.jpg",
+
+  "12.webp",
+  "13.webp",
+  "15.webp",
+  "3.jpg"
+];
