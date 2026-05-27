a5a4366ef817bf99dcb0c84d5d9b606b9fd46081
timat509 <mattisroy11@gmail.com>
2026-05-27 16:29:54 +0200

    Add client PIN downloads

---
+++ assets/albums/album-pins.js
@@ -0,0 +1,5 @@
+window.ALBUM_PINS = {
+  "frequencies_v1-16-05-26": "4466",
+  "roony_arlene-17-04-26": "2468",
+  "black_party-09-04-26": "4321"
+};
--- assets/albums/frequencies_v1-16-05-26/highlights.js
+++ assets/albums/frequencies_v1-16-05-26/highlights.js
@@ -1,6 +1,6 @@
 window.HIGHLIGHTS = [
   "IMG_1226.jpg",
   "IMG_1271.jpg",
-  "IMG_1202.jpg",
+  "IMG_1155.jpg",
   "IMG_1316.jpg"
 ];
